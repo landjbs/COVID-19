2f99243e16928cfc48f375b5d2a0797e578e4599
Landon Smith <landjbs@gmail.com>
2020-03-18 16:24:38 -0400

    article parsing

--- structs.py
+++ structs.py
@@ -1,13 +1,8 @@
 class Article(object):
     def __init__(self, id, authors, title, text):
         self.id = id
-        self.authors = authors
         self.title = title
         self.text = text
 
     def __str__(self):
-        return f'<{self.title} by {self.authors}>'
-
-    def author_str(self):
-        ''' Gets string of authors '''
-        
+        return f'<{self.title}>'
