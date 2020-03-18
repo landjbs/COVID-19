78b4d7fc43858c7e4b6edf7a9b359ea41b797dc4
Landon Smith <landjbs@gmail.com>
2020-03-18 18:17:36 -0400

    token_set

--- knowledge/knowledge_builder.py
+++ knowledge/knowledge_builder.py
@@ -28,7 +28,30 @@ import knowledge.knowledge_finder as knowledge_finder
 from utils import (save, load, safe_make_folder, delete_folder)
 
 
-def build_knowledge_set():
+def build_knowledge_set(top_path):
+    top_path = 'data/in_data/2020-03-13'
+    file_folders = ['bioxiv_medrxiv', 'comm_use_subset',
+                    'noncomm_use_subset', 'pmc_custom_license']
+    # set to hold all tokens
+    tokens = set()
+    for top_folder in tqdm(os.listdir(top_path)):
+        if top_folder in file_folders:
+            for path in os.listdir(f'{top_path}/{top_folder}/{top_folder}'):
+                if path.endswith('.json'):
+                    path = f'{top_path}/{top_folder}/{top_folder}/{path}'
+                    with open(path, 'r') as load_file:
+                        j = json.load(load_file)
+                        # get tokens from title
+                        for token in clean_title(j['metadata']['title']).split():
+                            tokens.add(token)
+                        # get tokens from abstract
+                        for paragraph in j['abstract']:
+                            for token in (clean_text(paragraph['text'])).split():
+                                tokens.add(token)
+                        # get tokens from body
+                        for paragraph in j['body_text']:
+                            for token in (clean_text(paragraph['text'])).split():
+                                tokens.add(token)
 
 
 
--- runner.py
+++ runner.py
@@ -8,32 +8,13 @@ import json
 from tqdm import tqdm
 from collections import Counter
 
+from utils import save_pickle
 from processing.cleaning import clean_text, clean_title
 
-top_path = 'data/in_data/2020-03-13'
-file_folders = ['bioxiv_medrxiv', 'comm_use_subset',
-                'noncomm_use_subset', 'pmc_custom_license']
+# dict mapping all ids to article url
 
-# set to hold all tokens
-tokens = set()
 
-for top_folder in tqdm(os.listdir(top_path)):
-    if top_folder in file_folders:
-        for path in os.listdir(f'{top_path}/{top_folder}/{top_folder}'):
-            if path.endswith('.json'):
-                path = f'{top_path}/{top_folder}/{top_folder}/{path}'
-                with open(path, 'r') as load_file:
-                    j = json.load(load_file)
-                    # get tokens from title
-                    for token in clean_title(j['metadata']['title']).split():
-                        tokens.add(token)
-                    # get tokens from abstract
-                    for paragraph in j['abstract']:
-                        for token in (clean_text(paragraph['text'])).split():
-                            tokens.add(token)
-                    # get tokens from body
-                    for paragraph in j['body_text']:
-                        for token in (clean_text(paragraph['text'])).split():
-                            tokens.add(token)
 print(len(tokens))
 print(tokens)
+
+save_pickle(tokens, 'data/out_data/token_set.save')
