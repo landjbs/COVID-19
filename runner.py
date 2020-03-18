e1f0d3e6d78303f296314febe65164b3bf05f95b
Landon Smith <landjbs@gmail.com>
2020-03-18 18:04:41 -0400

    token building init

--- runner.py
+++ runner.py
@@ -8,21 +8,32 @@ import json
 from tqdm import tqdm
 from collections import Counter
 
-all_words =
+from processing.cleaning import clean_text, clean_title
 
+top_path = 'data/in_data/2020-03-13'
 file_folders = ['bioxiv_medrxiv', 'comm_use_subset',
                 'noncomm_use_subset', 'pmc_custom_license']
-for top_folder in tqdm(os.listdir('2020-03-13')):
+
+# set to hold all tokens
+tokens = set()
+
+for top_folder in tqdm(os.listdir(top_path)):
     if top_folder in file_folders:
-        for path in os.listdir(f'2020-03-13/{top_folder}/{top_folder}'):
+        for path in os.listdir(f'{top_path}/{top_folder}/{top_folder}'):
             if path.endswith('.json'):
-                path = f'2020-03-13/{top_folder}/{top_folder}/{path}'
+                path = f'{top_path}/{top_folder}/{top_folder}/{path}'
                 with open(path, 'r') as load_file:
                     j = json.load(load_file)
-                    tokens = {}
-                    text = ''
+                    # get tokens from title
+                    for token in clean_title(j['metadata']['title']).split():
+                        tokens.add(token)
+                    # get tokens from abstract
                     for paragraph in j['abstract']:
-                        text += ' '.join(clean_text(paragraph['text']).split())
+                        for token in (clean_text(paragraph['text'])).split():
+                            tokens.add(token)
+                    # get tokens from body
                     for paragraph in j['body_text']:
-                        text += ' '.join(clean_text(paragraph['text']).split())
-                    
+                        for token in (clean_text(paragraph['text'])).split():
+                            tokens.add(token)
+print(len(tokens))
+print(tokens)
