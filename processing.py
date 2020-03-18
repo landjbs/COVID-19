49ecc420685c1129782b7669444fa896d8072020
Landon Smith <landjbs@gmail.com>
2020-03-18 16:39:57 -0400

    article parsing

--- processing.py
+++ processing.py
@@ -5,6 +5,7 @@ File processing to load database
 
 import os
 import json
+from tqdm import tqdm
 
 from structs import Article
 
@@ -13,21 +14,12 @@ file_folders = ['bioxiv_medrxiv', 'comm_use_subset',
                 'noncomm_use_subset', 'pmc_custom_license']
 
 
-def parse_article_json(j):
-    ''' Takes json of article j and returns processed object '''
-    id = j['paper_id']
-    meta = j['metadata']
-    title = meta['title']
-    abstract = j['abstract']
-    return Article(id=id, title=title, text=abstract)
-
-
-for top_folder in os.listdir('2020-03-13'):
+for top_folder in tqdm(os.listdir('2020-03-13')):
     if top_folder in file_folders:
         for path in os.listdir(f'2020-03-13/{top_folder}/{top_folder}'):
             if path.endswith('.json'):
                 path = f'2020-03-13/{top_folder}/{top_folder}/{path}'
                 with open(path, 'r') as load_file:
                     article_json = json.load(load_file)
-                    article_obj = parse_article_json(article_json)
-                    print(article_obj.text)
+                    article_obj = Article(article_json)
+                    # print(article_obj.abstract)
--- structs.py
+++ structs.py
@@ -3,7 +3,10 @@ class Article(object):
         self.id = j['paper_id']
         meta = j['metadata']
         self.title = meta['title']
-        self.abstract = j['abstract']
+        abstract = j['abstract']
+        self.abstract = abstract if (abstract!=[]) else None
+        for paragraph in j['body_text']:
+            
 
     def __str__(self):
         return f'<{self.title}>'
