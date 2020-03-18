40d318e7177afa606b12be2120d8f5d4e7d1cfb4
Landon Smith <landjbs@gmail.com>
2020-03-18 16:46:46 -0400

    article parsing

--- structs.py
+++ structs.py
@@ -1,3 +1,19 @@
+def clean_text(rawString):
+    """
+    Cleans rawString by replacing spaceMatcher and tagMatcher with a single
+    space, removing non-alpha chars, and lowercasing alpha chars
+    """
+    # replace accented characters with non-accent representation
+    deaccentedString = unidecode(rawString)
+    # replace stripMatcher with ""
+    cleanedString = re.sub(stripMatcher, "", rawString)
+    # replace spaceMatcher with " " and strip surround whitespace
+    spacedString = re.sub(spaceMatcher, " ", cleanedString).strip()
+    # lowercase the alpha chars that remain
+    loweredString = spacedString.lower()
+    return loweredString
+
+
 class Article(object):
     def __init__(self, j):
         self.id = j['paper_id']
@@ -17,5 +33,6 @@ class Article(object):
         print('\n'.join(self.paragraphs))
 
 
-# class Database(object):
-    # def __init__(self):
+class Database(object):
+    def __init__(self):
+        keywords = ['co-infections']
